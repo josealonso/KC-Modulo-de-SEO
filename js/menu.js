c496e64f8e2438e16ee91d1526eb4ec3bb7f465a
José Ramón Alonso Tapia <jose.alonso.programmer@gmail.com>
2018-03-07 16:55:21 +0100

    Como sugiere el informe de 'Gtmetrix', se usa el atributo 'defer' para acelerar la carga de la página.

--- js/menu.js
+++ js/menu.js
@@ -1,48 +1,3 @@
-/******************* Smooth scroll ******************/
-/*
-var navbarItems = document.getElementsByClassName('navbar-item');
-
-for (var i = 0; i < navbarItems.length; i++) {
-    navbarItems[i].addEventListener('click', function (event) {
-        var sectionToGo = this.getElementsByTagName('a')[0].href.split("#");
-
-        deleteActiveClass();
-        this.classList.add('active');
-        
-        if (sectionToGo.length === 2) { // If the link to go to is inside the page
-            event.preventDefault();
-            var goTo = sectionToGo[sectionToGo.length - 1];   // It removes the "#" character
-            getElementByIdAndScroll(goTo);
-        }
-    });
-}
-
-function getElementByIdAndScroll (id) {
-    var elem;
-    if (id === '') {
-        elem = document.getElementsByClassName('header')[0];
-    } else {
-        elem = document.getElementById(id);
-    }
-
-    scrollToElement(elem);
-}
-
-function scrollToElement (element) {
-    var jump = parseInt(element.getBoundingClientRect().top * 0.3);
-
-    //document.body.scrollTop += jump;
-    document.documentElement.scrollTop += jump;
-
-    if (!element.lastJump || element.lastJump > Math.abs(jump)) {
-        element.lastJump = Math.abs(jump);
-
-        setTimeout(function() {
-            scrollToElement(element);
-        }, 60);    // It was 40
-    } 
-}
-*/
 
 /******************* Smooth scroll with jQuery ******************/
 
@@ -81,7 +36,6 @@ var acumulativeOffset = function (element) {
     return top;
 }
 
-//var navbar = document.getElementsByClassName('navbar')[0]; 
 var navbarItems = document.getElementsByClassName('navbar-item');
 
 var offsetSummary = acumulativeOffset(document.getElementById('summary')) - 50;
